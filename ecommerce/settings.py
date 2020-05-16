ec4343f0b99cbe88939e731d3bed743ceb1648fa
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-05-16 13:55:39 +0000

    Added Heroku as allowed host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.gitpod.io']
+ALLOWED_HOSTS = ['.gitpod.io'], 'https://sah-ecommerce.herokuapp.com/'
 
 
 # Application definition
