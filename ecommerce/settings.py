3f3573d5ba421b2fab22d40c06df6de5a10d5612
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-05-16 14:31:05 +0000

    Amended Settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.gitpod.io', 'https://sah-ecommerce.herokuapp.com/']
+ALLOWED_HOSTS = ['.gitpod.io', 'sah-ecommerce.herokuapp.com']
 
 
 # Application definition
